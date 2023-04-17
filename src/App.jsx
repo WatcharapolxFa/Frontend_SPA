b6c256264722610cb42fdc1eea7bdf44ebee9469
WatcharapolxFa <watcharapolyotadee@outlook.com>
2023-04-18 02:55:06 +0700

    Success

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+import Avatar from './assets/man-avatar.webp'
+import './App.css'
+
+function App() {
+  const [user, setUser] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/user', { method: 'GET' })
+      .then(response => response.json())
+      .then(users => setUser(users.data))
+      .catch(error => console.log(error))
+  }, [])
+
+  return (
+    <div className="main--content">
+      {user && user.map(val => (
+        <div className='content--grid' key={val.email}>
+          <div className='content--left'>
+            <img alt='profile-picture' src={Avatar} />
+          </div>
+          <div className='content--right'>
+            <h1>{val?.name}</h1>
+            <div className='content--right_detail'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className='icon-red'>
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+              </svg>
+              <h3>{val?.email}</h3>
+            </div>
+            <div className='content--right_detail'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className='icon-green'>
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+              </svg>
+              <h3>{val?.dob}</h3>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
